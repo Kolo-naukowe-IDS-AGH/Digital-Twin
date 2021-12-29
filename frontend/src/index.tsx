1fe05694709520f3e6de45ea2349247d6b9d505f
dominikwozniak <degie999@gmail.com>
2021-12-29 23:19:22 +0100

    FE - format files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "styles/styles-core.scss";
 import HomePage from "./pages/Home";
 import NotFoundPage from "./pages/404";
@@ -14,13 +10,8 @@ ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<MainTemplate />}>
-        <Route
-          index
-          element={<HomePage />} />
-        <Route
-          path="*"
-          element={<NotFoundPage />}
-        />
+        <Route index element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   </BrowserRouter>,
--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const HomePage: React.FC = () => {
-  return (
-    <div>
-      Home
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default HomePage;
